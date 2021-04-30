cb41d2a70e65e52e00d09ff0d3c5b0fa3dea2cd7
Bhumit Malvi <bhumitmalvi999@gmail.com>
2021-04-30 15:07:47 +0530

    added  sites

--- webnovels/scraping/models.py
+++ webnovels/scraping/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class sites():
+    id : int
+    title: str
+    link: str
--- webnovels/scraping/views.py
+++ webnovels/scraping/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import sites
+import mysql.connector
 
 def login(request):
     return render(request,'login.html')
 
-
-
 def scrap(request):
     if request.POST["username"] == 'bhumit' and request.POST["password"] =='bhum1234':
-        return render(request,'scrapper.html')
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="bhumit",
+        password="admin123",
+        database="novels"
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute("select * from websites;")
+        results = mycursor.fetchall()
+        sitelist = []
+        for result in results:
+            site = sites()
+            site.id,site.title,site.link = result
+            sitelist.append(site)
+        return render(request,'scraper.html',{'websites':sitelist})
     else:
         return HttpResponse('wrong username and password')
