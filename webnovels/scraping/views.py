69d04622bcf19f8b59b69397c59a0696d4987b25
Bhumit Malvi <bhumitmalvi999@gmail.com>
2021-04-30 14:09:36 +0530

    added admin page

--- webnovels/scraping/views.py
+++ webnovels/scraping/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def login(request):
     return render(request,'login.html')
+
+
+
+def scrap(request):
+    if request.POST["username"] == 'bhumit' and request.POST["password"] =='bhum1234':
+        return render(request,'scrapper.html')
+    else:
+        return HttpResponse('wrong username and password')
